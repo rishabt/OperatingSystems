d27fde12fbbcdf708c5839de9065dc8f6e88608d
Rishabh Tandon <rishabh.tandon@mail.mcgill.ca>
2014-12-01 19:24:09 -0500

    Fix bug

--- FileSystem/sfs_api.c
+++ FileSystem/sfs_api.c
@@ -10,68 +10,81 @@ void mksfs(int fresh)
 	if(fresh)
 	{
 		// Initialize fresh disk
-		int init = init_fresh_disk("root_dir.sfs", BLOCKSIZE, DISKSIZE);
+		init_fresh_disk("root_dir.sfs", BLOCKSIZE, DISKSIZE);
 
-		if(init)
-		{
-			int i;
-			for(i = 1; i < MAXIMUM_FILES; i++) {			// Initialize all files to empty
-				root.directory_table[i].isEmpty = 1;
-			}
-
-			root.next = 0;									// Cursor set to 0
-
-			for(i = 0; i < DISKSIZE; i++) {					// Initialize all blocks to free
-				freeList.freeblocks[i] = 0;
-			}
-
-			for(i = 0; i < DISKSIZE; i++) {
-				FAT.fatNodes[i].index = EMPTY;
-				FAT.fatNodes[i].next = EMPTY;
-			}
-
-			FAT.next = 0;									// Cursor set to 0
-
-			// Write blocks for root, fat, and free blocks
-			write_blocks(0, 1, (void *)&root);
-			write_blocks(1, 1, (void *)&FAT);
-			write_blocks(DISKSIZE-1, 1, (void *)&freeList);
+		int i;
+		for(i = 1; i < MAXIMUM_FILES; i++) {			// Initialize all files to empty
+			root.directory_table[i].isEmpty = 1;
 		}
+
+		root.next = 0;									// Cursor set to 0
+
+		for(i = 0; i < DISKSIZE; i++) {					// Initialize all blocks to free
+			freeList.freeblocks[i] = 0;
+		}
+
+		for(i = 0; i < DISKSIZE; i++) {
+			FAT.fatNodes[i].index = EMPTY;
+			FAT.fatNodes[i].next = EMPTY;
+		}
+
+		FAT.next = 0;									// Cursor set to 0
+
+		// Write blocks for root, fat, and free blocks
+		write_blocks(0, 1, (void *)&root);
+		write_blocks(1, 1, (void *)&FAT);
+		write_blocks(DISKSIZE-1, 1, (void *)&freeList);
+
 	}
 	else
 	{
 		// Initialize disk
-		int init = init_disk("root_dir.sfs", BLOCKSIZE, DISKSIZE);
+		init_disk("root_dir.sfs", BLOCKSIZE, DISKSIZE);
 
-		if(init)
-		{
-			// Since it's not a fresh file system, read existing blocks
-		    read_blocks(0, 1, (void *)&root);
-		    read_blocks(1, 1, (void *)&FAT);
-		    read_blocks(DISKSIZE-1, 1, (void *)&freeList);
-		}
+		// Since it's not a fresh file system, read existing blocks
+		read_blocks(0, 1, (void *)&root);
+		read_blocks(1, 1, (void *)&FAT);
+		read_blocks(DISKSIZE-1, 1, (void *)&freeList);
+
 	}
 }
 
 void sfs_ls(void)
 {
-	int i;
-	// Print name, size (in KB, divide by 1000) and time (convert to c-string to make it readable)
-	for(i = 0; root.directory_table[i].isEmpty == 0; i++)
-	{
-		printf("%s  %dKB  %s", root.directory_table[i].name, (root.directory_table[i].size/1000),
-				ctime(&root.directory_table[i].creation_time));
+	int i = 0;
 
+	while(i < MAXIMUM_FILES)
+	{
+		if(root.directory_table[i].isEmpty == 0)
+		{
+			printf("%s  %dKB  %s", root.directory_table[i].name, (root.directory_table[i].size/1000),
+							ctime(&root.directory_table[i].creation_time));
+		}
+		i++;
 	}
+
+	printf("FILES %d", i);
+	// Print name, size (in KB, divide by 1000) and time (convert to c-string to make it readable)
+//	for(i = 0; root.directory_table[i].isEmpty == 0; i++)
+//	{
+//		printf("%s  %dKB  %s", root.directory_table[i].name, (root.directory_table[i].size/1000),
+//				ctime(&root.directory_table[i].creation_time));
+//
+//	}
 }
 
 int sfs_fopen(char *name)
 {
 	int fileID = isFileOpen(name);
 
+	if(fileID != -1)
+	{
+		return fileID;
+	}
+	printf("HERE 1\n");
 	int fileIndex = getFileIndex(name);
 
-	strcpy( fdt[opened_files].filename, name );
+	strcpy(fdt[opened_files].filename, name);
 	fdt[opened_files].opened = 1;
 	fdt[opened_files].read_ptr = 0;
 
